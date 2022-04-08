46bc3b11a332626df3d069721aaa190f8c1f92cd
Yusen Zheng <ysyszheng@gmail.com>
2022-04-08 22:41:19 +0800

    update project

--- Project/code/easy.m
+++ Project/code/easy.m
@@ -1,6 +1,26 @@
-function licenseNumber = easy(path)
+function licenseNumber = easy(path, whiteCountPerColumnThreshold, verbose)
+%EASY 用于识别easy模式中车牌的简单算法
+%   author: Yusen Zheng
+%   input1: path: 车牌图像的路径
+%   input2: whiteCountPerColumnThreshold: 白色像素点数目的阈值
+%   input3: verbose: 是否打印详细信息
+%   output: licenseNumber: 车牌号码 
 
-%% 字符分割
+    %% default value
+
+    switch nargin
+        case 1
+            whiteCountPerColumnThreshold = 5;
+            verbose = true;
+        case 2
+            verbose = true;
+        case 3
+            % do nothing
+        otherwise
+            error('Wrong number of input arguments');
+    end 
+
+    %% 字符分割
 
     % 灰度、滤波与二值化
     licensePlate = imread(path);
@@ -21,39 +41,61 @@ function licenseNumber = easy(path)
     
     % 确定兴趣域
     whiteCountPerRow = sum(licensePlateBW,2);
-    regions = whiteCountPerRow > 10; %TODO
-    startIdx = find(diff(regions)==1);
-    endIdx = find(diff(regions)==-1);
+    whiteCountPerRowThreshold = .5*mean(whiteCountPerRow);
+    regionsRow = whiteCountPerRow > whiteCountPerRowThreshold;
+    startIdx = find(diff(regionsRow)==1);
+    endIdx = find(diff(regionsRow)==-1);
     licenseNumberBW = licensePlateBW(startIdx:endIdx,:);
-    licenseNumberROI = licensePlateBW(2:end-1,2:end-1);
-    whiteCountPerColumn = sum(licenseNumberROI,1);
+    licenseNumberROI = [zeros(size(licenseNumberBW,1),1) licenseNumberBW zeros(size(licenseNumberBW,1),1)];
     
     % 字符分割及错误分割的处理
-    whiteCountPerColumnThreshold = 5;
-    wrongDivisionThreshold = 15;
-
+    gap = startIdx(2:end) - endIdx(1:end-1);
+    wrongDivisionThreshold = .5*mean(gap);
+    
     % 存在字符的区域
-    regions = whiteCountPerColumn > whiteCountPerColumnThreshold;
+    whiteCountPerColumn = sum(licenseNumberROI,1);
+    regionsColumn = whiteCountPerColumn > whiteCountPerColumnThreshold;
     
     % 处理错误的分割（分割了原子字符）
-    startIdx = find(diff(regions)==1);
-    endIdx = find(diff(regions)==-1);
-    wrongDivisionIdx = find(startIdx(2:end)-endIdx(1:end-1) < wrongDivisionThreshold);
-    regions(endIdx(wrongDivisionIdx):startIdx(wrongDivisionIdx+1)) = 1;
+    startIdx = find(diff(regionsColumn)==1);
+    endIdx = find(diff(regionsColumn)==-1);
+    wrongDivisionIdx = find(gap < wrongDivisionThreshold);
+    regionsColumn(endIdx(wrongDivisionIdx):startIdx(wrongDivisionIdx+1)) = 1;
     
     % 向后差分判断分割域
-    startIdx = find(diff(regions)==1);
-    endIdx = find(diff(regions)==-1);
+    startIdx = find(diff(regionsColumn)==1);
+    endIdx = find(diff(regionsColumn)==-1);
     regions = endIdx-startIdx;
-    widthThreshold = mean(regions);
+    widthThreshold = .5*mean(regions);
 
     % 丢弃分隔符
     del = find(regions<widthThreshold);
     startIdx(del) = [];
     endIdx(del) = [];
     regions(del) = [];
-    
-    %% *字符识别*
+
+    % 单字符可视化
+    if verbose
+        figure
+        whiteCountPerColumn = sum(licenseNumberROI,1);
+        imshow(licenseNumberROI)
+        hold on
+        plot(max(whiteCountPerColumn) - whiteCountPerColumn,'r',"LineWidth",3)
+        grid on
+        axis tight
+        hold off
+
+        figure
+        tiledlayout(1,size(regions,2))
+        for i = 1:size(regions,2)
+            letterImage = licenseNumberROI(:,startIdx(i):endIdx(i));
+            nexttile
+            imshow(letterImage)
+        end
+    end
+
+    %% 字符识别
+
     % 导入模板字符
     templateDir = fullfile('./templates');
     templates = dir(fullfile(templateDir,'*.bmp'));
